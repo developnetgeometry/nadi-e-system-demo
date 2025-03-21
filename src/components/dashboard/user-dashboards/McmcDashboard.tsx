84c18a88305b04b713efcc36f1587a375c4227ac
AcapXasyraF <acapxasyrafx@yahoo.com>
2025-03-21 09:28:47 +0800

    Fix: -added dashboard based on user type. - add case match filter to detect usertype

--- src/components/dashboard/user-dashboards/McmcDashboard.tsx
+++ src/components/dashboard/user-dashboards/McmcDashboard.tsx
@@ -4,22 +4,23 @@ import { DashboardMap } from "@/components/dashboard/DashboardMap";
 import { useDashboardData } from "@/hooks/use-dashboard-data";
 import { Shield } from "lucide-react";
 
-export const AdminDashboard = () => {
+export const McmcDashboard = () => {
   const { data: stats, isLoading, error } = useDashboardData();
 
   if (error) {
-    console.error("TP dashboard data error:", error);
+    console.error("MCMC dashboard data error:", error);
   }
 
   return (
     <div className="space-y-8">
       <div className="flex items-center gap-4 mb-4">
         <Shield className="h-8 w-8 text-primary" />
-        <h1 className="text-3xl font-bold">TP Dashboard</h1>
+        <h1 className="text-3xl font-bold">MCMC Dashboard</h1>
       </div>
 
       <DashboardStats stats={stats} isLoading={isLoading} />
       <DashboardMap />
+      <DashboardCharts />
     </div>
   );
 };
