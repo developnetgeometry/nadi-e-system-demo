ac5908ccf415028cb69b65ed3216e142a5e5c808
aan-cloud <evirsanti@gmail.com>
2025-05-07 16:12:12 +0800

    feat: add more small component to separate functional logic and data

--- src/components/site/BookingManagementDetail.tsx
+++ src/components/site/BookingManagementDetail.tsx
@@ -129,3 +129,247 @@ const FacilityCalender = ({value}) => {
         </TabsContent>
     )
 }
+
+const PcMainContent = () => {
+
+    return (
+        <section className="mt-6 flex flex-col" id="pc status">
+            <PcStatus />
+            <ResentPcBooking />
+        </section>
+    )
+}
+
+const PcStatus = () => {
+
+    const statusBadges = [
+        {
+            name: "Available",
+            customClass: "bg-green-200 text-green-600 hover:bg-green-300 font-semibold"
+        },
+        {
+            name: "In Use",
+            customClass: "bg-blue-200 text-blue-600 hover:bg-blue-300 font-semibold"
+        },
+        {
+            name: "Maintenance",
+            customClass: "bg-red-200 text-red-600 hover:bg-red-300 font-semibold"
+        }
+    ];
+
+    // Upcoming function to get pcs data
+    // const { data, error, isLoading } = usePcsData();
+    const Pcs = [
+        {
+            status: 'available',
+            type: 'gaming',
+            name: 'PC001',
+            spec: 'RTX3060, i7',
+            staffName: 'Farhan',
+            startDate: '14:30',
+            duration: '1h 30m',
+            icon: <Server/>,
+            customClass: "bg-green-200 text-green-600 hover:bg-green-300 font-semibold"
+        },
+        {
+            status: 'available',
+            type: 'gaming',
+            name: 'PC002',
+            spec: 'RTX3060, i7',
+            staffName: 'Farhan',
+            startDate: '14:30',
+            duration: '1h 30m',
+            icon: <Server/>,
+            customClass: "bg-green-200 text-green-600 hover:bg-green-300 font-semibold"
+        },{
+            status: 'available',
+            type: 'gaming',
+            name: 'PC003',
+            spec: 'RTX3060, i7',
+            staffName: 'Farhan',
+            startDate: '14:30',
+            duration: '1h 30m',
+            icon: <Server/>,
+            customClass: "bg-green-200 text-green-600 hover:bg-green-300 font-semibold"
+        },{
+            status: 'available',
+            type: 'gaming',
+            name: 'PC004',
+            spec: 'RTX3060, i7',
+            staffName: 'Farhan',
+            startDate: '14:30',
+            duration: '1h 30m',
+            icon: <Server/>,
+            customClass: "bg-green-200 text-green-600 hover:bg-green-300 font-semibold"
+        },{
+            status: 'in-use',
+            type: 'gaming',
+            name: 'PC005',
+            spec: 'RTX3060, i7',
+            staffName: 'Farhan',
+            startDate: '14:30',
+            duration: '1h 30m',
+            icon: <Server/>,
+            customClass: "bg-blue-200 text-blue-600 hover:bg-blue-300 font-semibold"
+        },{
+            status: 'in-use',
+            type: 'gaming',
+            name: 'PC006',
+            spec: 'RTX3060, i7',
+            staffName: 'Farhan',
+            startDate: '14:30',
+            duration: '1h 30m',
+            icon: <Server/>,
+            customClass: "bg-blue-200 text-blue-600 hover:bg-blue-300 font-semibold"
+        },{
+            status: 'in-use',
+            type: 'standard',
+            name: 'PC007',
+            spec: 'RTX3060, i7',
+            staffName: 'Farhan',
+            startDate: '14:30',
+            duration: '1h 30m',
+            icon: <Server/>,
+            customClass: "bg-blue-200 text-blue-600 hover:bg-blue-300 font-semibold"
+        },{
+            status: 'in-use',
+            type: 'standard',
+            name: 'PC008',
+            spec: 'RTX3060, i7',
+            staffName: 'Farhan',
+            startDate: '14:30',
+            duration: '1h 30m',
+            icon: <Server/>,
+            customClass: "bg-blue-200 text-blue-600 hover:bg-blue-300 font-semibold"
+        },
+    ];
+
+    const [pcs, setPcs] = useState([]);
+
+    function onFilterChange({ pcAvailability, pcTypeTabs, searchQuery }: FilterParams) {
+        let filtered = Pcs;
+
+        if (pcAvailability !== "all") {
+            filtered = filtered.filter((pc) => pc.status === pcAvailability);
+        }
+
+        if (pcTypeTabs !== "all") {
+            filtered = filtered.filter((pc) => pc.type === pcTypeTabs);
+        }
+
+        if (!!searchQuery) {
+            filtered = filtered.filter((pc) => pc.name.includes(searchQuery));
+        }
+
+        setPcs(filtered);
+    }
+
+    useEffect(() => {
+        onFilterChange({ pcAvailability:"all", pcTypeTabs: "all"})
+    }, []);
+
+    return (
+        <>
+            <div className="flex justify-between">
+                <h1 className="text-2xl font-bold">PC Status</h1>
+                <div className="flex gap-2">
+                    {statusBadges.map(({ name, customClass}) => (
+                        <Badge key={name} className={`${customClass}`}>{name}</Badge>
+                    ))}
+                </div>
+            </div>
+            <div>
+                <FilterBar className="flex mt-6 justify-between" onFilterChange={onFilterChange} showDateRange={false} showRegion={false} showCenterType={false} showPcBookingFilter={true} />
+                <div className="grid grid-cols-4 gap-5 mt-4">
+                        {
+                            pcs.map((pc) => (
+                                <DataCard 
+                                    title={pc.type} 
+                                    value={pc.name} 
+                                    description={pc.spec}
+                                    icon={pc.icon}
+                                    name={pc.staffName}
+                                    label={( <Badge className={`${pc.customClass}`}>{pc.status}</Badge> )}
+                                    footer={`started: ${pc.startDate} Duration: ${pc.duration}`}
+                                />
+                            ))
+                        }
+                </div>
+            </div>
+        </>
+    )
+}
+
+const ResentPcBooking = () => {
+
+    const headTable = ["User", "PC", "Start Time", "End Time", "Duration"];
+
+    // Upcoming custom hooks to get PC booking data
+    // const { data, isLoading, error } = useBookingPc();
+    const recentBooking = [
+        {
+            userName: "siti aminah",
+            pcName: "PC-001",
+            startTime: "14:30",
+            endTime: "16:00",
+            duration: "1h 30m"
+        },
+        {
+            userName: "siti aminah",
+            pcName: "PC-001",
+            startTime: "14:30",
+            endTime: "16:00",
+            duration: "1h 30m"
+        },{
+            userName: "siti aminah",
+            pcName: "PC-001",
+            startTime: "14:30",
+            endTime: "16:00",
+            duration: "1h 30m"
+        },{
+            userName: "siti aminah",
+            pcName: "PC-001",
+            startTime: "14:30",
+            endTime: "16:00",
+            duration: "1h 30m"
+        },{
+            userName: "siti aminah",
+            pcName: "PC-001",
+            startTime: "14:30",
+            endTime: "16:00",
+            duration: "1h 30m"
+        }
+    ];
+
+    return (
+        <div className="mt-8 space-y-7 overflow-hidden">
+                <h1 className="font-bold text-2xl text-center">Recent Booking</h1>
+                <div className="overflow-hidden rounded-xl border border-gray-200 shadow-sm">
+                <Table className="w-full table-auto border-collapse text-sm">
+                <TableHeader>
+                    <TableRow>
+                        {
+                            headTable.map((head) => (
+                                <TableHead key={head}>{head}</TableHead>
+                            ))
+                        }
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {
+                        recentBooking.map((bookingItem, index) => (
+                            <TableRow className="hover:bg-gray-200">
+                                <TableCell>{bookingItem.userName}</TableCell>
+                                <TableCell>{bookingItem.pcName}</TableCell>
+                                <TableCell>{bookingItem.startTime}</TableCell>
+                                <TableCell>{bookingItem.endTime}</TableCell>
+                                <TableCell>{bookingItem.duration}</TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+                </Table>
+                </div>
+            </div>
+    )
+}
