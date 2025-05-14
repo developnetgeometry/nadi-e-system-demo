643fb175adad32d64beb0038bffa621713da8288
aan-cloud <evirsanti@gmail.com>
2025-05-14 14:19:23 +0800

    feat: role base content access content and request new booking

--- src/components/site/BookingManagementDetail.tsx
+++ src/components/site/BookingManagementDetail.tsx
@@ -6,12 +6,17 @@ import { Badge } from "../ui/badge";
 import DataCard from "./component/DataCard";
 import { useEffect, useState } from "react";
 import { useAssets } from "@/hooks/use-assets";
-import { UseQueryResult } from "@tanstack/react-query";
-import { Asset } from "@/types/asset";
-import { useBookingMutation } from "@/hooks/booking/use-booking-mutation";
 import { BookingListsTable } from "./component/BookingListTable";
 import { BookingCalendar } from "./component/BookingCalendar";
 import { useBookingQueries } from "@/hooks/booking/use-booking-queries";
+import { Booking } from "@/types/booking";
+import { toTwentyFourFormat } from "./utils/dateTimeConverter";
+import { getDuration } from "./utils/duration";
+import { Asset } from "@/types/asset";
+import { useUserMetadata } from "@/hooks/use-user-metadata";
+import { useSiteId } from "@/hooks/use-site-id";
+import { LoadingSpinner } from "../shared/LoadingSpinner";
+import { EmptyCard } from "./component/EmptyCard";
 
 type FilterParams = {
     pcAvailability: string,
@@ -40,10 +45,66 @@ const BookingHeader = () => {
 }
 
 const BookingContent = () => {
-    const { useAssetsByTypeQuery } = useAssets();
+    // Set UI content based on role access modifier
+    const userMetadata = useUserMetadata();
+    const parsedMetadata = userMetadata ? JSON.parse(userMetadata) : null;
+    const organizationId =
+        parsedMetadata?.user_type !== "super_admin" &&
+            parsedMetadata?.user_group_name === "TP" || parsedMetadata?.user_group_name === "Site" &&
+            parsedMetadata?.organization_id
+            ? parsedMetadata.organization_id
+            : null;
+    const isTp = !!organizationId;
+    console.log("Organitation id", organizationId)
+
+    const { useAssetsByTypeQuery, useAssetsInTpsSites } = useAssets();
+    const { useBookingQuery, useTpsSites, useBookingAssetInTpsSites } = useBookingQueries();
 
     const pcIdAsset = 1;
-    const { data: pcs = [], isLoading } = useAssetsByTypeQuery(pcIdAsset);
+    const { data: memberSitePcs = [], isLoading: isMemberSitePcsLoading } = useAssetsByTypeQuery(pcIdAsset);
+    const { data: tpsSites, isLoading: isTpsSitesLoading } = useTpsSites(organizationId);
+    console.log("tps sites", tpsSites)
+    const { data: tpsSitesPcs, isLoading: isAssetTpsSitesLoading } = useAssetsInTpsSites(tpsSites?.map(tp => tp.id), pcIdAsset);
+    console.log("tpsistes pcs", tpsSitesPcs)
+    /**
+     * 
+     * 
+     *   14 - 17 May
+     * - Facility
+     * - Book Facility
+     */
+
+    const pcAssetTypeId = 1;
+    const { data: pcsBooking, isLoading: isBookingLoading } = useBookingQuery(pcAssetTypeId);
+    const { data: tpsSitesPcsBookings, isLoading: isTpsSitesPcsBookingsLoading } = useBookingAssetInTpsSites(tpsSites?.map(tp => tp.id), pcAssetTypeId);
+    console.log("tps sites booking", tpsSitesPcsBookings)
+    if (
+        isTpsSitesLoading ||
+        isMemberSitePcsLoading ||
+        isAssetTpsSitesLoading ||
+        isBookingLoading ||
+        !userMetadata ||
+        !parsedMetadata ||
+        isTpsSitesPcsBookingsLoading
+    ) {
+        return <LoadingSpinner />;
+    }
+
+
+    const tpsSitespcsAvailibility = tpsSitesPcs?.map((pc) => {
+        const currentBooking = pc.nd_booking?.find((b) => {
+            const now = new Date();
+            return new Date(b.booking_start) <= now && new Date(b.booking_end) >= now;
+        });
+
+        const fallbackBooking = pc.nd_booking?.at(-1);
+
+        const booking = currentBooking || fallbackBooking || null;
+
+        return {
+            is_using: !!booking?.is_using
+        };
+    });
 
     return (
         <Tabs defaultValue="PC Bookings" className="w-full grid place-items-center mt-7">
@@ -54,23 +115,28 @@ const BookingContent = () => {
                     ))
                 }
             </TabsList>
-            <PcBookings 
-                value="PC Bookings" 
+            <PcBookings
+                value="PC Bookings"
                 pcStats={{
-                    totalPcs: isLoading ? "Loading..." : pcs.length,
-                    pcInUse: 0,
-                    pcAvailable: 0
+                    totalPcs: isTp ? tpsSitesPcs.length : memberSitePcs.length,
+                    pcInUse: isTp ? tpsSitespcsAvailibility.filter(pc => pc.is_using).length : memberSitePcs.filter(pc => pc.is_active).length,
+                    pcAvailable: isTp ? tpsSitespcsAvailibility.filter(pc => !pc.is_using).length : memberSitePcs.filter(pc => !pc.is_active).length
                 }}
+                pcsData={isTp ? tpsSitesPcs : memberSitePcs}
+                bookingsData={isTp ? tpsSitesPcsBookings : pcsBooking}
+                isLoading={isBookingLoading}
             />
-            <PcCalender 
-                value="PC Calendar" 
-                pcsData={pcs.map((pc) => pc.name)}
+            <PcCalender
+                value="PC Calendar"
+                pcsData={isTp ? tpsSitesPcs.map((pc) => pc.name) : memberSitePcs.map((pc) => pc.name)}
+                bookingsData={isTp && !!tpsSitesPcsBookings ? tpsSitesPcsBookings : pcsBooking}
+                isLoading={isBookingLoading}
             />
-            <FacilityBooking 
-                value="Facility Bookings" 
+            <FacilityBooking
+                value="Facility Bookings"
             />
-            <FacilityCalender 
-                value="Facility Calendar" 
+            <FacilityCalender
+                value="Facility Calendar"
             />
         </Tabs>
     )
@@ -80,12 +146,21 @@ interface PcBookingProps {
     value: string,
     pcStats: {
         totalPcs: number | string,
-        pcInUse: number,
-        pcAvailable: number
+        pcInUse: number | string,
+        pcAvailable: number | string
     }
+    pcsData: Asset[],
+    bookingsData: Booking[]
+    isLoading: boolean
 }
 
-const PcBookings = ({value, pcStats}: PcBookingProps) => {
+const PcBookings = ({
+    value,
+    pcStats,
+    pcsData,
+    bookingsData,
+    isLoading
+}: PcBookingProps) => {
 
     const statsItems = [
         {
@@ -103,7 +178,7 @@ const PcBookings = ({value, pcStats}: PcBookingProps) => {
             description: "",
             iconBgColor: "bg-red-100",
             iconTextColor: "text-red-500",
-        },{
+        }, {
             title: "Available",
             value: String(pcStats.pcAvailable),
             icon: CircleCheckBig,
@@ -122,40 +197,85 @@ const PcBookings = ({value, pcStats}: PcBookingProps) => {
                     ))
                 }
             </section>
-            <PcMainContent />
+            <PcMainContent
+                pcsData={pcsData}
+                bookingsData={bookingsData}
+                isLoading={isLoading}
+            />
         </TabsContent>
+    )
+}
+
+interface PcMainContentProps {
+    bookingsData: Booking[]
+    pcsData: Asset[]
+    isLoading: boolean
+}
+
+export const PcMainContent = ({
+    bookingsData,
+    pcsData,
+    isLoading
+}: PcMainContentProps) => {
+    const headTable = ["User", "PC", "Start Time", "End Time", "Duration"];
+
+    const recentBookings = bookingsData.map((booking) => {
+        return {
+            userName: booking.profiles.full_name,
+            bookingAssetTypeName: booking.nd_asset.name,
+            startTime: toTwentyFourFormat(new Date(booking.booking_start)),
+            endTime: toTwentyFourFormat(new Date(booking.booking_end)),
+            duration: getDuration(booking.booking_start, booking.booking_end),
+            ...booking
+        }
+    }).filter((booking) => booking.is_using === true)
+
+    return (
+        <section className="mt-6 flex flex-col" id="pc status">
+            <PcStatus
+                pcsData={pcsData}
+                isLoading={isLoading}
+            />
+            <BookingListsTable headTable={headTable} bodyTableData={recentBookings} />
+        </section>
     )
 }
 
 interface PcCalenderProps {
     value: string,
-    pcsData: string[]
+    pcsData: string[],
+    bookingsData: Booking[],
+    isLoading: boolean
 }
 
-const PcCalender = ({
-    value, 
-    pcsData
+export const PcCalender = ({
+    value,
+    pcsData,
+    bookingsData,
+    isLoading
 }: PcCalenderProps) => {
-    // Handle post new booking
-    const { useAssetMutation } = useBookingMutation();
 
-    const pcAssetTypeId = 1;
-    const { useBookingQuery } = useBookingQueries();
-    const { data: pcsBooking, isLoading } = useBookingQuery(pcAssetTypeId);
+    const [bookingCalendarData, setBookingCalendarData] = useState([]);
 
     const onChangeFilter = (date: Date, assetTypeName: string) => {
+        let filteredBooking = bookingsData.filter((booking) => new Date(booking.created_at).getDay() === date.getDay())
 
+        if (assetTypeName !== "all pc") {
+            filteredBooking = filteredBooking.filter((booking) => booking.nd_asset.name.toLowerCase() === assetTypeName.toLowerCase())
+        }
+
+        setBookingCalendarData(filteredBooking)
     }
 
     return (
         <TabsContent className="w-full" value={value}>
-            <BookingCalendar 
+            <BookingCalendar
                 assetTypeNames={[
                     "all pc",
                     ...pcsData
                 ]}
                 bookingType="pc"
-                bookingData={pcsBooking}
+                bookingData={bookingCalendarData}
                 isLoading={isLoading}
                 onChangeFilter={onChangeFilter}
             />
@@ -163,7 +283,7 @@ const PcCalender = ({
     )
 }
 
-const FacilityBooking = ({value}) => {
+const FacilityBooking = ({ value }) => {
     return (
         <TabsContent value={value}>
             {/* just for an example content */}
@@ -172,7 +292,7 @@ const FacilityBooking = ({value}) => {
     )
 }
 
-const FacilityCalender = ({value}) => {
+const FacilityCalender = ({ value }) => {
     return (
         <TabsContent value={value}>
             {/* just for an example content */}
@@ -181,55 +301,15 @@ const FacilityCalender = ({value}) => {
     )
 }
 
-const PcMainContent = () => {
-    const headTable = ["User", "PC", "Start Time", "End Time", "Duration"];
-
-    // Upcoming custom hooks to get PC booking data
-    // const { data, isLoading, error } = useBookingPc();
-    const recentBooking = [
-        {
-            userName: "siti aminah",
-            bookingAssetTypeName: "PC-001",
-            startTime: "14:30",
-            endTime: "16:00",
-            duration: "1h 30m"
-        },
-        {
-            userName: "siti aminah",
-            bookingAssetTypeName: "PC-001",
-            startTime: "14:30",
-            endTime: "16:00",
-            duration: "1h 30m"
-        },{
-            userName: "siti aminah",
-            bookingAssetTypeName: "PC-001",
-            startTime: "14:30",
-            endTime: "16:00",
-            duration: "1h 30m"
-        },{
-            userName: "siti aminah",
-            bookingAssetTypeName: "PC-001",
-            startTime: "14:30",
-            endTime: "16:00",
-            duration: "1h 30m"
-        },{
-            userName: "siti aminah",
-            bookingAssetTypeName: "PC-001",
-            startTime: "14:30",
-            endTime: "16:00",
-            duration: "1h 30m"
-        }
-    ];
-
-    return (
-        <section className="mt-6 flex flex-col" id="pc status">
-            <PcStatus />
-            <BookingListsTable headTable={headTable} bodyTableData={recentBooking}/>
-        </section>
-    )
+interface PcStatusProps {
+    pcsData: Asset[]
+    isLoading: boolean
 }
 
-const PcStatus = () => {
+const PcStatus = ({
+    pcsData,
+    isLoading
+}: PcStatusProps) => {
 
     const statusBadges = [
         {
@@ -246,97 +326,44 @@ const PcStatus = () => {
         }
     ];
 
-    // Upcoming function to get pcs data
-    // const { data, error, isLoading } = usePcsData();
-    const Pcs = [
-        {
-            status: 'available',
-            type: 'gaming',
-            name: 'PC001',
-            spec: 'RTX3060, i7',
-            staffName: 'Farhan',
-            startDate: '14:30',
-            duration: '1h 30m',
-            icon: <Server/>,
-            customClass: "bg-green-200 text-green-600 hover:bg-green-300 font-semibold"
-        },
-        {
-            status: 'available',
-            type: 'gaming',
-            name: 'PC002',
-            spec: 'RTX3060, i7',
-            staffName: 'Farhan',
-            startDate: '14:30',
-            duration: '1h 30m',
-            icon: <Server/>,
-            customClass: "bg-green-200 text-green-600 hover:bg-green-300 font-semibold"
-        },{
-            status: 'available',
-            type: 'gaming',
-            name: 'PC003',
-            spec: 'RTX3060, i7',
-            staffName: 'Farhan',
-            startDate: '14:30',
-            duration: '1h 30m',
-            icon: <Server/>,
-            customClass: "bg-green-200 text-green-600 hover:bg-green-300 font-semibold"
-        },{
-            status: 'available',
-            type: 'gaming',
-            name: 'PC004',
-            spec: 'RTX3060, i7',
-            staffName: 'Farhan',
-            startDate: '14:30',
-            duration: '1h 30m',
-            icon: <Server/>,
-            customClass: "bg-green-200 text-green-600 hover:bg-green-300 font-semibold"
-        },{
-            status: 'in-use',
-            type: 'gaming',
-            name: 'PC005',
-            spec: 'RTX3060, i7',
-            staffName: 'Farhan',
-            startDate: '14:30',
-            duration: '1h 30m',
-            icon: <Server/>,
-            customClass: "bg-blue-200 text-blue-600 hover:bg-blue-300 font-semibold"
-        },{
-            status: 'in-use',
-            type: 'gaming',
-            name: 'PC006',
-            spec: 'RTX3060, i7',
-            staffName: 'Farhan',
-            startDate: '14:30',
-            duration: '1h 30m',
-            icon: <Server/>,
-            customClass: "bg-blue-200 text-blue-600 hover:bg-blue-300 font-semibold"
-        },{
-            status: 'in-use',
-            type: 'standard',
-            name: 'PC007',
-            spec: 'RTX3060, i7',
-            staffName: 'Farhan',
-            startDate: '14:30',
-            duration: '1h 30m',
-            icon: <Server/>,
-            customClass: "bg-blue-200 text-blue-600 hover:bg-blue-300 font-semibold"
-        },{
-            status: 'in-use',
-            type: 'standard',
-            name: 'PC008',
-            spec: 'RTX3060, i7',
-            staffName: 'Farhan',
-            startDate: '14:30',
-            duration: '1h 30m',
-            icon: <Server/>,
-            customClass: "bg-blue-200 text-blue-600 hover:bg-blue-300 font-semibold"
-        },
-    ];
+    const pcsAvailibility = pcsData?.map((pc) => {
+        const currentBooking = pc.nd_booking?.find((b) => {
+            const now = new Date();
+            return new Date(b.booking_start) <= now && new Date(b.booking_end) >= now;
+        });
+
+        const fallbackBooking = pc.nd_booking?.at(-1);
+
+        const booking = currentBooking || fallbackBooking || null;
+
+        const isBooking = !!booking;
+        const startDate = (start: string) => {
+            if (isBooking && start) {
+                return new Date(booking.booking_start).toLocaleTimeString();
+            } else {
+                return "-"
+            }
+        }
+
+        return {
+            status: booking?.is_using ? "in-use" : "Available",
+            type: pc.nd_brand?.brand_type,
+            name: pc.name,
+            spec: pc.nd_brand?.name,
+            staffName: isBooking ? booking.created_by ?? "-" : "-",
+            startDate: startDate(booking?.booking_start),
+            duration: isBooking && getDuration(booking?.booking_start, booking?.booking_end),
+            icon: <Server />,
+            customClass: !isBooking
+                ? "bg-green-200 text-green-600 hover:bg-green-300 font-semibold"
+                : "bg-blue-200 text-blue-600 hover:bg-blue-300 font-semibold",
+        };
+    });
 
     const [pcs, setPcs] = useState([]);
 
     function onFilterChange({ pcAvailability, pcTypeTabs, searchQuery }: FilterParams) {
-        let filtered = Pcs;
+        let filtered = pcsAvailibility;
 
         if (pcAvailability !== "all") {
             filtered = filtered.filter((pc) => pc.status === pcAvailability);
@@ -354,7 +381,7 @@ const PcStatus = () => {
     }
 
     useEffect(() => {
-        onFilterChange({ pcAvailability:"all", pcTypeTabs: "all"})
+        onFilterChange({ pcAvailability: "all", pcTypeTabs: "all" })
     }, []);
 
     return (
@@ -362,28 +389,35 @@ const PcStatus = () => {
             <div className="flex justify-between">
                 <h1 className="text-2xl font-bold">PC Status</h1>
                 <div className="flex gap-2">
-                    {statusBadges.map(({ name, customClass}) => (
+                    {statusBadges.map(({ name, customClass }) => (
                         <Badge key={name} className={`${customClass}`}>{name}</Badge>
                     ))}
                 </div>
             </div>
             <div>
                 <FilterBar className="flex mt-6 justify-between" onFilterChange={onFilterChange} showDateRange={false} showRegion={false} showCenterType={false} showPcBookingFilter={true} />
-                <div className="grid grid-cols-4 gap-5 mt-4">
-                        {
-                            pcs.map((pc) => (
-                                <DataCard 
-                                    title={pc.type} 
-                                    value={pc.name} 
-                                    description={pc.spec}
-                                    icon={pc.icon}
-                                    name={pc.staffName}
-                                    label={( <Badge className={`${pc.customClass}`}>{pc.status}</Badge> )}
-                                    footer={`started: ${pc.startDate} Duration: ${pc.duration}`}
-                                />
-                            ))
-                        }
-                </div>
+                {
+                    pcs.length === 0 ? (
+                        <EmptyCard message="There are no PC in your sites" className="mt-6 py-30"/>
+                    ) : (
+                        <div className="grid grid-cols-4 gap-5 mt-4">
+                            {
+                                pcs.map((pc, i) => (
+                                    <DataCard
+                                        title={pc.type}
+                                        key={pc.name + i}
+                                        value={pc.name}
+                                        description={pc.spec}
+                                        icon={pc.icon}
+                                        name={pc.staffName}
+                                        label={(<Badge className={`${pc.customClass}`}>{pc.status}</Badge>)}
+                                        footer={`started: ${pc.startDate} Duration: ${pc.duration}`}
+                                    />
+                                ))
+                            }
+                        </div>
+                    )
+                }
             </div>
         </>
     )
