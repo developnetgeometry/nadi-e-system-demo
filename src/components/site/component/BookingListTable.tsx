d3496673ef56e852ff8f39674aee8f43ccaa8ff2
aan-cloud <evirsanti@gmail.com>
2025-05-16 17:36:36 +0800

    feat: add no booking found component

--- src/components/site/component/BookingListTable.tsx
+++ src/components/site/component/BookingListTable.tsx
@@ -6,6 +6,8 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
+import { NoBookingFound } from "./NoBookingFound";
+import { FolderXIcon } from "lucide-react";
 
 interface BookingListTableProps {
     headTable: string[],
@@ -25,34 +27,41 @@ export const BookingListsTable = ({
 
     return (
         <div className="mt-8 space-y-7 overflow-hidden">
-                <h1 className="font-bold text-2xl text-center">Recent Booking</h1>
-                <div className="overflow-hidden rounded-xl border border-gray-200 shadow-sm">
-                <Table className="w-full table-auto border-collapse text-sm">
-                <TableHeader>
-                    <TableRow>
+            <h1 className="font-bold text-2xl text-center">Recent Booking</h1>
+            <div>
+                <Table className="w-full table-auto border-collapse text-sm bg-white rounded-md">
+                    <TableHeader>
+                        <TableRow>
+                            {
+                                headTable.map((head) => (
+                                    <TableHead key={head}>{head}</TableHead>
+                                ))
+                            }
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
                         {
-                            headTable.map((head) => (
-                                <TableHead key={head}>{head}</TableHead>
+                            bodyTableData.map((bookingItem, index) => (
+                                <TableRow key={index} className="hover:bg-gray-200">
+                                    <TableCell className="font-semibold">{bookingItem.userName}</TableCell>
+                                    <TableCell>{bookingItem.bookingAssetTypeName}</TableCell>
+                                    <TableCell>{bookingItem.startTime}</TableCell>
+                                    <TableCell>{bookingItem.endTime}</TableCell>
+                                    <TableCell>{bookingItem.duration}</TableCell>
+                                </TableRow>
                             ))
                         }
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {/* SOON: UI for 0 booking */}
-                    {
-                        bodyTableData.map((bookingItem, index) => (
-                            <TableRow className="hover:bg-gray-200">
-                                <TableCell>{bookingItem.userName}</TableCell>
-                                <TableCell>{bookingItem.bookingAssetTypeName}</TableCell>
-                                <TableCell>{bookingItem.startTime}</TableCell>
-                                <TableCell>{bookingItem.endTime}</TableCell>
-                                <TableCell>{bookingItem.duration}</TableCell>
-                            </TableRow>
-                        ))
-                    }
-                </TableBody>
+                    </TableBody>
                 </Table>
-                </div>
+                { bodyTableData.length === 0 && (
+                    <NoBookingFound
+                        description="No Recent Booking"
+                        icon={<FolderXIcon className="mx-auto mb-2 h-10 w-10 text-gray-500" />}
+                        title="No PC Booking Recently"
+                        className="w-full bg-white rounded-md mt-3 py-6 border border-gray-300"
+                    />
+                )}
             </div>
+        </div>
     )
 }
