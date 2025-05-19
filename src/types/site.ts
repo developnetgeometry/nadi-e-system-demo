45893c0699c4c5f1f71366f535cdc92d11e8ccc9
aan-cloud <evirsanti@gmail.com>
2025-05-19 16:51:43 +0800

    chore: nested type

--- src/types/site.ts
+++ src/types/site.ts
@@ -1,3 +1,5 @@
+import { SiteProfile } from "@/components/member/hook/useSiteProfile";
+
 export interface Site {
   id: number;
   sitename: string;
@@ -165,4 +167,18 @@ export interface Socioeconomic {
 export interface Space {
   id: string;
   eng: string;
+  created_by?: string;
+  created_at?: string | Date;
+  updated_by?: string;
+  updated_at?: string | Date;
+}
+
+export interface SiteSpace {
+  id: number
+  nd_space?: Space
+  nd_site_profile?: SiteProfile
+  created_at?: string | Date,
+  updated_at?: string | Date,
+  created_by?: string,
+  updated_by?: string
 }
